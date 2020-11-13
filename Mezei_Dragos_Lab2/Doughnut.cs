a5033568d2299f4a208739e86cc5fe18dac37be1
paraZite <paraZite@DESKTOP-2U8TDJM>
2020-11-13 17:17:51 +0200

    Implementare functionalitate lab3

---
+++ Mezei_Dragos_Lab2/Doughnut.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mezei_Dragos_Lab2
+{
+    class Doughnut
+    {
+        private DoughnutType mFlavor;
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+
+        private float mPrice;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+
+        public Doughnut(DoughnutType aFlavor)
+        {
+            mFlavor = aFlavor;
+        }
+    }
+
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+}
